273d54eb6c6e81ec7f9120e2432786431fefcb21
Lars Anderson <ishka912@gmail.com>
2011-03-10 04:34:35 +0000

    + trying to clean up repo - moving class files to trunk/classes

---
+++ Classes/LACallButton.h
@@ -0,0 +1,16 @@
+//
+//  LACallButton.h
+//  DeceptiCam
+//
+//  Created by Lars Anderson on 3/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LACallButton : UIButton {
+
+}
+
+@end
