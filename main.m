a802d0c6ddf062bbbc47eb2689afdfaae46204a7
Lars Anderson <ishka912@gmail.com>
2011-03-10 04:13:10 +0000

    + moving decepticam data to trunk

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DeceptiCam
+//
+//  Created by Lars Anderson on 2/19/11.
+//  Copyright 2011 Drink & Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
